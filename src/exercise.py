1c467f73ab2234ff495ce1c80d9e2a196394c6f1
den01-student <student@den01.co.uk>
2020-05-15 12:28:01 +0000

    Attempted solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,30 @@
+from random import randint
+
 def main():
-    #write your code below this line
+
+    lower_bound = 1
+    upper_bound = 10
+
+    print("I'm going to guess a number between " + str(lower_bound) + " and " + str(upper_bound) + ".")
+    guess = randint(lower_bound,upper_bound)
+
+    while True:
+
+      print("I guessed " + str(guess) + ".")
+      
+      correct = input("Was I correct? ")
+      if correct == "You got it right":
+        print("Woo!")
+        return
+      elif correct == "Too high!":
+        guess = randint(lower_bound,guess)
+      elif correct == "Too low!":
+        guess = randint(guess,upper_bound)
+      else:
+        print("Sorry I didn't quite catch that!")
+
+    # note - this is not a very clever solution. There are lots of ways that this could be improved!
+
 
 if __name__ == '__main__':
     main()
